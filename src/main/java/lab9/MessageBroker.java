22a2b4bb9ef49becf1bc0d7aeee28b1895b854a4
vostolaza <victor.ostolaza@utec.edu.pe>
2020-10-14 15:45:19 -0500

    Message Queue

--- src/main/java/lab9/MessageBroker.java
+++ src/main/java/lab9/MessageBroker.java
@@ -1,1 +1,35 @@
 package lab9;
+
+class MessageBroker {
+    HashMap<String, List<Subscriber>> subscriberMap;
+    HashMap<String, List<String>> messageMap;
+
+    public MessageBroker(){
+        subscriberMap = new HashMap<String, List<Subscriber>>();
+        messageMap = new HashMap<String, List<String>>();
+    }
+
+    public void registerSubscriber(Subscriber s, String channel){
+        var subscriberList = subscriberMap.get(channel);
+        if (subscriberList != null){
+            subscriberList.add(s);
+        }
+        else{
+            subscriberList = new ArrayList<String>();
+            subscriberList.add(s);
+            subscriberMap.put(channel, subscriberList);
+        }
+    }
+
+    public void removeSubscriber(Subscriber s, String channel){
+        var subscriberList = subscriberMap.get(channel);
+        subscriberList.remove(s);
+    }
+
+    public void notifySubscribers(String channel){
+        var subscriberList = subscriberMap.get(channel);
+        for (Subscriber s : subscriberList){
+            s.notify();
+        }
+    }
+}
