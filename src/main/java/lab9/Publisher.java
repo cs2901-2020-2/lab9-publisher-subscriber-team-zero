9e76d7bd0769d601b163e6279fd179d70d0e8a04
Christian Rojas <christian.rojas@utec.edu.pe>
2020-10-14 10:22:17 -0500

    struct +

--- src/main/java/lab9/Publisher.java
+++ src/main/java/lab9/Publisher.java
@@ -1,1 +1,10 @@
 package lab9;
+
+public class Publisher(){
+    public void publish(Object data, String channel){
+        List<Suscriber> listSubs = map.get(channel);
+        for (Suscriber sub : listSubs){
+            sub.notify();
+        }
+    }
+}
---
+++ src/main/java/lab9/Subscriber.java
@@ -0,0 +1,7 @@
+package lab9;
+
+public class Suscriber(){
+
+    public void subscribe(String channel);
+    public void notify ();
+}
